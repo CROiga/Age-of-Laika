ccd04e785ce51fdc805945fb50f16e6c42b2eb4b
Igor Majer <CROiga@Igors-MacBook-Pro.local>
2014-10-06 19:56:10 +0200

    Initial Commit

---
+++ Age of Laika/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Age of Laika
+//
+//  Created by Igor Majer on 6.10.2014..
+//  Copyright (c) 2014. Igor Majer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
